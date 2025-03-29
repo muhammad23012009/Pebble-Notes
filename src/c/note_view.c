c01e34671edc40ccdf71a5b867a334f6a6100821
Muhammad <thevancedgamer@mentallysanemainliners.org>
2025-03-29 14:15:49 +0500

    note_view: Add scrolling support make it look better on chalk
    Signed-off-by: Muhammad <thevancedgamer@mentallysanemainliners.org>

--- src/c/note_view.c
+++ src/c/note_view.c
@@ -3,14 +3,51 @@
 static void prv_select_handler(ClickRecognizerRef ref, void *context)
 {
     NoteView *view = (NoteView*) context;
-    APP_LOG(APP_LOG_LEVEL_DEBUG, "Testing select handler, %p, %p, %d", view, view->note, view->note->index);
+    storage_delete_note_from_watch(view->note->index);
     notes_data_remove_note(view->data, view->note->index);
     window_stack_pop(true);
+}
+
+static void prv_scroll_handler(ClickRecognizerRef ref, void *context)
+{
+    NoteView *view = (NoteView*) context;
+
+    GPoint scroll_point = scroll_layer_get_content_offset(view->scroll_layer);
+    GSize scroll_size = scroll_layer_get_content_size(view->scroll_layer);
+
+    ButtonId id = click_recognizer_get_button_id(ref);
+    if (id == BUTTON_ID_UP) {
+        if (!gpoint_equal(&scroll_point, &GPointZero)) {
+            if (scroll_point.y > -16)
+                scroll_layer_set_content_offset(view->scroll_layer, GPointZero, true);
+            else
+                scroll_layer_set_content_offset(view->scroll_layer, GPoint(0, scroll_point.y + 16), true);
+        }
+    } else if (id == BUTTON_ID_DOWN) {
+        scroll_layer_set_content_offset(view->scroll_layer, GPoint(0, scroll_point.y - 16), true);
+    }
 }
 
 static void prv_click_config_provider(void *context)
 {
     window_single_click_subscribe(BUTTON_ID_SELECT, prv_select_handler);
+    window_single_click_subscribe(BUTTON_ID_UP, prv_scroll_handler);
+    window_single_click_subscribe(BUTTON_ID_DOWN, prv_scroll_handler);
+}
+
+static void prv_create_action_bar(NoteView *view)
+{
+    view->action_layer = action_bar_layer_create();
+    action_bar_layer_set_context(view->action_layer, view);
+    action_bar_layer_set_click_config_provider(view->action_layer, prv_click_config_provider);
+
+    view->delete_icon = gbitmap_create_with_resource(RESOURCE_ID_DELETE_ICON);
+    view->up_icon = gbitmap_create_with_resource(RESOURCE_ID_UP_ICON);
+    view->down_icon = gbitmap_create_with_resource(RESOURCE_ID_DOWN_ICON);
+
+    action_bar_layer_set_icon(view->action_layer, BUTTON_ID_SELECT, view->delete_icon);
+    action_bar_layer_set_icon(view->action_layer, BUTTON_ID_UP, view->up_icon);
+    action_bar_layer_set_icon(view->action_layer, BUTTON_ID_DOWN, view->down_icon);
 }
 
 static void prv_window_load(Window *window)
@@ -26,25 +63,25 @@ static void prv_window_load(Window *window)
     GRect status_bounds = layer_get_bounds(status_bar_layer_get_layer(view->status_layer));
 
     GRect bounds = GRect(0, status_bounds.size.h, window_bounds.size.w, window_bounds.size.h - status_bounds.size.h);
-    view->scroll_layer = scroll_layer_create(bounds);
+    view->scroll_layer = scroll_layer_create(window_bounds);
+    scroll_layer_set_shadow_hidden(view->scroll_layer, true);
+    scroll_layer_set_frame(view->scroll_layer, bounds);
 
-    GRect text_bounds = GRect(3, 0, bounds.size.w - 30, bounds.size.h);
-    GSize text_size = graphics_text_layout_get_content_size(view->note->note_text, font, text_bounds, GTextOverflowModeWordWrap, GTextAlignmentLeft);
+    GRect text_bounds = GRect(PBL_IF_ROUND_ELSE(3, 2),
+                              PBL_IF_ROUND_ELSE(30, 0),
+                              bounds.size.w - ACTION_BAR_WIDTH - PBL_IF_ROUND_ELSE(3, 2), 2000);
 
     view->text_layer = text_layer_create(text_bounds);
     text_layer_set_text(view->text_layer, view->note->note_text);
     text_layer_set_font(view->text_layer, font);
     text_layer_set_overflow_mode(view->text_layer, GTextOverflowModeWordWrap);
-    scroll_layer_set_content_size(view->scroll_layer, text_size);
+    GSize text_size = text_layer_get_content_size(view->text_layer);
 
-    layer_add_child(scroll_layer_get_layer(view->scroll_layer), text_layer_get_layer(view->text_layer));
+    scroll_layer_set_content_size(view->scroll_layer, GSize(text_size.w, text_size.h + PBL_IF_ROUND_ELSE(90, 30)));
 
-    view->action_layer = action_bar_layer_create();
-    action_bar_layer_set_context(view->action_layer, view);
-    action_bar_layer_set_click_config_provider(view->action_layer, prv_click_config_provider);
+    scroll_layer_add_child(view->scroll_layer, text_layer_get_layer(view->text_layer));
 
-    view->delete_icon = gbitmap_create_with_resource(RESOURCE_ID_DELETE_ICON);
-    action_bar_layer_set_icon(view->action_layer, BUTTON_ID_SELECT, view->delete_icon);
+    prv_create_action_bar(view);
 
     layer_add_child(window_layer, scroll_layer_get_layer(view->scroll_layer));
     action_bar_layer_add_to_window(view->action_layer, window);
@@ -60,8 +97,12 @@ static void prv_window_unload(Window *window)
     action_bar_layer_destroy(view->action_layer);
 
     gbitmap_destroy(view->delete_icon);
+    gbitmap_destroy(view->up_icon);
+    gbitmap_destroy(view->down_icon);
 
     free(view);
+    view = NULL;
+    window_destroy(window);
 }
 
 NoteView *note_view_create(Note *note, NotesData *data)
@@ -69,7 +110,6 @@ NoteView *note_view_create(Note *note, NotesData *data)
     NoteView *view = malloc(sizeof(NoteView));
     view->note = note;
     view->data = data;
-    APP_LOG(APP_LOG_LEVEL_DEBUG, "meow meow meow %p", view->note);
     view->window = window_create();
 
     window_set_user_data(view->window, view);
